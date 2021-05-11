2e0352cc9c344c1c002c12900fe49ce02348d65f
user name <email@domain.com>
2021-05-12 01:16:24 +0400

    2&3 puls check finish

--- app.js
+++ app.js
@@ -1,26 +1,54 @@
 const express = require("express");
+const fs = require("fs");
+const axios = require("axios");
 const app = express();
 const port = 2500;
 
 // a middleware that enables us to read the received JSON data
 app.use(express.json());
 
-const users = [
-  { name: "John", age: 25 },
-  { name: "Jane", age: 20 },
-  { name: "Mark", age: 19 },
-];
 
-// a GET request on endpoint http://localhost:3000/users
-app.get("/users", (req, res) => {
-  // set the response status code to 200 (OK)
-  res.status(200);
-  // sends back a response of all users
-  res.json(users);
-});
+
+const readFile = () => {
+    // TODO: Your code here
+    fs.readFile("data.txt" , (err , data) => {
+        if(err) {throw err } 
+        else { console.log("Data" , data.toString() )}
+    })
+};
+
+readFile()
+
+
+
+const writeFile = () => {
+    // TODO: Your code here
+    fs.writeFile("text.txt", "A new file has been created", (err) => {
+        if (err) throw err;
+        console.log("The file has been saved");
+    });
+    
+    
+};
+
+
+writeFile()
+
+
+
+
+
+
+
+
+
+
 
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
+
 
